bdbb518059b86a6711e35e28f0e3b13d6fa09423
Thomas Smajstrla <smajstrla@google.com>
2019-07-18 09:32:32 -0500

    changed location to Austin

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 30.2672, lng: 97.7431},
+      center: {lat: 30.2672, lng: -97.7431},
       zoom: 8
     });
   }
